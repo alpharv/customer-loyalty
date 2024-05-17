4a10ca28a40625308762fc5a67cc4da836867f55
Roland Villemoes <rv@alpha-solutions.us>
2024-05-17 11:17:02 -0400

    Minor fix

--- lib/cryptoHelper.ts
+++ lib/cryptoHelper.ts
@@ -3,10 +3,12 @@ export const VerifyRequest = (
     requestBody: string,
     requestHash: string | undefined
 ): boolean => {
+    console.log('VerifyRequest');
     if (typeof requestHash === "undefined") {
     return false;
     }
-    const hashKey: string = process.env.VERIFICATION_KEY;
+    const hashKey: string = process.env.VERIFICATION_KEY as string;
+    console.log('VerifyRequest:hashKey=' + hashKey);
     const bodyBytes: Buffer = Buffer.from(requestBody, "utf-8");
     const keyBytes: Buffer = Buffer.from(hashKey, "utf-8");
     const hash = crypto
