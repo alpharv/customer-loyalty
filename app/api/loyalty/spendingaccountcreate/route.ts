9b1a85dc94f8f0f0739ae8ab359a8be43d044b22
Roland Villemoes <rv@alpha-solutions.us>
2024-09-15 21:10:49 -0400

    Minor updates on spending accounts (draft!)

---
+++ app/api/loyalty/spendingaccountcreate/route.ts
@@ -0,0 +1,29 @@
+import { VerifyRequest } from "@/lib/cryptoHelper";
+import { baseOrderCloud } from "@/types/ordercloud/requestBodies";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+    if (process.env.REACT_APP_LOGGING === "true") {
+        console.log(request.headers);   
+        console.log(request.body);   
+    }
+    var VerificationHeaderName = process.env.VERIFICATION_HEADER as string
+    if (!VerifyRequest(
+            JSON.stringify(request.body),
+            request.headers.get(VerificationHeaderName) as string)  
+    )
+    {
+        if (process.env.REACT_APP_LOGGING === "true") 
+            console.log('Unauthorized');
+        return Response.json(JSON.parse('{"result":"Unauthorized (spendingaccountassigned)"}'), { status: 401 })
+    }
+
+    const webhookRequest = request.body as unknown as baseOrderCloud;
+    switch (webhookRequest.Verb) {
+    //case "XXXXX": TODO
+    //    break;
+
+    default:
+        return Response.json(JSON.parse('{"result":"Default! OK! (spendingaccountassigned)"}'), { status: 200 })
+    }  
+}
--- lib/cryptoHelper.ts
+++ lib/cryptoHelper.ts
@@ -3,12 +3,11 @@ export const VerifyRequest = (
     requestBody: string,
     requestHash: string | undefined
 ): boolean => {
-    console.log('VerifyRequest');
+   console.log('VerifyRequest: requestHash='+requestHash);
     if (typeof requestHash === "undefined") {
     return false;
     }
     const hashKey: string = process.env.VERIFICATION_KEY as string;
-    console.log('VerifyRequest:hashKey=' + hashKey);
     const bodyBytes: Buffer = Buffer.from(requestBody, "utf-8");
     const keyBytes: Buffer = Buffer.from(hashKey, "utf-8");
     const hash = crypto
